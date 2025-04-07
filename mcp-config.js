b864934b092594f126a666144f731f20976a3d52
Mykal Thomas <mykal.thomas@avetta.com>
2025-04-07 16:02:22 -0500

    Initial commit

---
+++ mcp-config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    mcp_github: {
+        command: "npx",
+        args: ["-y", "@modelcontextprotocol/server-github"],
+    },
+};
